00b81d8038d299e8c63f98bbe0d7f2d311821c1f
TUNG N KIM <Tungkims@gmail.com>
2023-02-14 15:05:31 -0600

    Added code of unit testing to placeholder tests and TODO comments

--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -16,29 +16,85 @@ namespace CPW211_UnitTestStarterCode.Tests
         [DataRow(0, 100)]
         [DataRow(-1, -10)]
         [DataRow(0, -0)]
+        [TestCategory("Add")]
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
-            // Use the DataRow values to test the Add method
-            Assert.Fail();
+            // Arrange 
+            double expectedValue = num1 + num2;
+
+            // Act
+            double returnValue = SimpleMath.Add(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expectedValue, returnValue);
+        }
+
+
+        [TestMethod]
+        [DataRow(2, .5)]
+        [DataRow(100, .5)]
+        [DataRow(-2, 5.5)]
+        [DataRow(-50, -2.5)]
+        [TestCategory("Multiply")]
+        public void Multiply_TwoNumbers_ReturnsProduct(double num1, double num2)
+        {
+            // Arrange 
+            double expectedValue = num1 * num2;
+
+            // Act
+            double returnValue = SimpleMath.Multiply(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expectedValue, returnValue);
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(2)]
+        [DataRow(100)]
+        [DataRow(-2)]
+        [DataRow(-50)]
+        [TestCategory("Divide")]
+        public void Divide_DenominatorZero_ThrowsArgumentException(double num1)
         {
-            // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
-        }
-
-        [TestMethod]
-        public void Divide_DenominatorZero_ThrowsArgumentException()
-        {
-            // Divide by zero should throw an argument exception with a message
-            // "Denominator cannot be zero"
-            Assert.Fail();
+            Assert.ThrowsException<ArgumentException>(() => SimpleMath.Divide(num1, 0));
         }
 
         // TODO: Test Divide method with two valid numbers
+        [TestMethod]
+        [DataRow(2, .5)]
+        [DataRow(100, .5)]
+        [DataRow(-2, 5.5)]
+        [DataRow(-50, 1000)]
+        [TestCategory("Divide")]
+        public void Divide_TwoValidNumbers_ReturnsProduct(double num1, double num2)
+        {
+            // Arrange 
+            double expectedValue = num1 / num2;
+
+            // Act
+            double returnValue = SimpleMath.Divide(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expectedValue, returnValue);
+        }
 
         // TODO: Test subtract method with two valid numbers
+        [TestMethod]
+        [DataRow(2, 5)]
+        [DataRow(-100, -105)]
+        [DataRow(-1, 99.5)]
+        [DataRow(-1000, .01)]
+        [TestCategory("Subtract")]
+        public void Subtract_TwoValidNumbers_ReturnsProduct(double num1, double num2)
+        {
+            // Arrange 
+            double expectedValue = num1 - num2;
+
+            // Act
+            double returnValue = SimpleMath.Subtract(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expectedValue, returnValue);
+        }
     }
 }
